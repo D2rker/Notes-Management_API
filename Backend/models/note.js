0225f32ab296083877bcd0fea987789b30092585
vikas kimothi <[vikikimothi13dv@gmail.com]>
2026-05-30 15:58:15 +0530

    first commit

--- Backend/models/note.js
+++ Backend/models/note.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> a66c37d8a7acebbdab129ccb4fbde8199ca73585
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
@@ -14,15 +11,12 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide note content'],
     trim: true,
-<<<<<<< HEAD
   },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Note', noteSchema);
-=======
   }
   
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Note', noteSchema);
->>>>>>> a66c37d8a7acebbdab129ccb4fbde8199ca73585
